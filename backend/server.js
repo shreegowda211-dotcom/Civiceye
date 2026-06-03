fb4e051be582a707e877932b0272f45fa7fa0af7
Shree <shreegowda211@gmail.com>
2026-06-04 00:39:41 +0530

    Enable CORS preflight handling
    Added CORS preflight handling for all routes.

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
